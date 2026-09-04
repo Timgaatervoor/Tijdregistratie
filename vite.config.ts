fb28f01f69a2748aae230b9af67100fa2fec3121
Timgaatervoor <72581909+Timgaatervoor@users.noreply.github.com>
2026-09-05 00:39:40 +0200

    install via git

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Tijdregistratie/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {

7b8fc47ca9ac401cf486964164b83eedfdf9e87b
Timgaatervoor <72581909+Timgaatervoor@users.noreply.github.com>
2026-09-07 23:04:43 +0200

    tijd uitklapbaar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dh6f3ct8v44"
+    "revision": "0.81cgi0ehb4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
